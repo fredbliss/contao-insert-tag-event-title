80e3ead02740fc8833f91710d88ae63aefe1222d
fbliss <fred.bliss@intelligentspark.com>
2017-05-24 17:13:10 -0400

    added autoloads, fixed bugs

---
+++ config/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    'IntelligentSpark\Hooks\EventTitle' => 'system/modules/contao-insert-tag-event-title/library/IntelligentSpark/Hooks/EventTitle.php'
+));
